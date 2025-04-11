d57fc7d13f7c613da47ad76b4fc1b61547ab1433
Khamdy <jeremy.khamdy@epitech.eu>
2025-04-11 12:09:50 +0200

    [FIX] wrong import from analytics next =/= nuxt

--- application/app.vue
+++ application/app.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
+import { Analytics } from '@vercel/analytics/nuxt';
 import { useState } from 'nuxt/app';
 import { portfolio } from './data/portfolio';
 const portfolioData = useState('portfolioData',  () => portfolio.getData());
 </script>
 <template>
+    <Analytics />
     <!-- <NuxtRouteAnnouncer /> -->
     <!-- <NuxtWelcome /> -->
      <NuxtLayout>
--- application/layouts/default.vue
+++ application/layouts/default.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 import type { Portfolio } from '../types/data';
-import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/vue';
 import { useState } from 'nuxt/app';
 
@@ -8,7 +7,6 @@ const data = useState<Portfolio>('portfolioData');
 </script>
 
 <template>
-    <Analytics />
     <SpeedInsights />
     <div class="min-h-screen min-w-full scroll-smooth flex flex-col justify-between">
         <AppHeader :portfolio="data"/>
